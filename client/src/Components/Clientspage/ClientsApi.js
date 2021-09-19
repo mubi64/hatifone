8f11f31ee33b5dd37cfd1316dcfadfda63316a00
saadsafdar <72243425+saadsafda@users.noreply.github.com>
2021-09-19 23:05:59 +0500

    make it better

---
+++ client/src/Components/Clientspage/ClientsApi.js
@@ -0,0 +1,37 @@
+import PropsCom from "./PropsCom";
+
+const ClientsApi = () => {
+  return (
+    <>
+      <PropsCom images="images/bma.png" styles="30px 0px" />
+      <PropsCom images="images/ary.jpg" styles="26px 0px" />
+      <PropsCom images="images/attock.jpg" styles="8px 0px" />
+      <PropsCom images="images/pizza.jpg" styles="8px 20px" />
+      <PropsCom images="images/arysa.png" styles="8px 18px" />
+      <PropsCom images="images/bpizza.png" styles="10px 0px" />
+      <PropsCom images="images/king.png" styles="0px 0px" />
+      <PropsCom images="images/clucky.jpg" styles="16px 22px" />
+      <PropsCom images="images/cpizza.png" styles="16px 22px" />
+      <PropsCom images="images/back.png" styles="16px 0px" />
+      <PropsCom images="images/Kbrost.jpg" styles="16px 22px" />
+      <PropsCom images="images/jaldi.png" />
+      <PropsCom images="images/food.jpg" />
+      <PropsCom images="images/Dvago.png" />
+      <PropsCom images="images/go.png" styles="40px 0px" />
+      <PropsCom images="images/kay.jpg" />
+      <PropsCom images="images/pizzac.jpg" />
+      <PropsCom images="images/pizzahut.png" styles="18px 0px" />
+      <PropsCom images="images/max.png" />
+      <PropsCom images="images/lal.png" styles="60px 0px" />
+      <PropsCom images="images/un.png" styles="34px 0px" />
+      <PropsCom images="images/sab.png" styles="34px 0px" />
+      <PropsCom images="images/pizzamax.png" styles="60px 0px" />
+      <PropsCom images="images/pizzapoint.png" styles="18px 24px" />
+      <PropsCom images="images/si.png" styles="32px 0px" />
+      <PropsCom images="images/V.png" styles="20px 24px" />
+      <PropsCom images="images/wa.png" styles="34px 0px" />
+    </>
+  );
+};
+
+export default ClientsApi;
